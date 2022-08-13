0c4ebc0848ab6541430ebb818d841f54c73d987f
Doux Salem kayembe <76118979+douxsalemk@users.noreply.github.com>
2022-08-13 14:35:38 -0300

    feat: use gorilla mux

---
+++ README.md
@@ -0,0 +1,4 @@
+### REST-API-GO
+
+Este é meu quarto projeto em GO.
+Foi usado o framwork GIN para implementar a api rest
--- routes/routes.go
+++ routes/routes.go
@@ -4,12 +4,15 @@ import (
 	"log"
 	"net/http"
 	"rest-api-go/controllers"
+
+	"github.com/gorilla/mux"
 )
 
 
-
 func HandleResquest() {
-	http.HandleFunc("/", controllers.Home)
-	http.HandleFunc("/api/personalidades", controllers.TodasPersonalidades)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	r := mux.NewRouter()
+	r.HandleFunc("/", controllers.Home)
+	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
+	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
