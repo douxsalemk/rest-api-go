2295fc74e2f867c643827caa6b3f841b9d892cd4
Doux Salem kayembe <76118979+douxsalemk@users.noreply.github.com>
2022-08-13 14:51:53 -0300

    feat: use gorm to acess database

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"rest-api-go/database"
 	"rest-api-go/models"
 	"rest-api-go/routes"
 )
@@ -11,7 +12,7 @@ func main() {
 		{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
 		{Id: 2, Nome: "Nome 2", Historia: "Historia 2"},
 	}
-
+	database.ConectaComBancoDeDados()
 	fmt.Println("Iniciando o servidor Rest com Go")
 	routes.HandleResquest()
 }
